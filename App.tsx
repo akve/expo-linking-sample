3b2d5bbabf461b8b157c618342beaf116c88c615
alex postnikov <postnikov@gmail.com>
2023-06-20 16:29:31 +0300

    initial

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import { Linking } from 'react-native'; 
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text onPress={()=>{
+        Linking.openURL('mailto:billgates@gmail.com');
+      }}>Try linking!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
